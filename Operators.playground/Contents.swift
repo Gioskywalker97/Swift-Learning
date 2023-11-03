81724b01e66dcb496addf5bfe246834c33059920
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2023-11-03 08:01:25 -0400

    Operators Learning

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let myAge = 22
+let yourAge = 20
+
+if myAge > yourAge {
+    "I'm Older then you"
+} else if myAge < yourAge {
+    "I'm younger then you"
+} else {
+    "We are the same age"
+}
--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Vandoo"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+names.append("Bar")
+names.append("Baz")
+
+let oldArray = NSMutableArray(
+    array: [
+    "Foo",
+    "Bar"
+    ]
+)
+oldArray.add("Baz")
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
