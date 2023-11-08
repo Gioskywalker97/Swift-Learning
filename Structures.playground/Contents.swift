f82f8de211cac9a992086bb4c353ab0c37b81cb2
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2023-11-08 07:42:52 -0500

    Learning Structures

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+struct Person {
+    let name: String
+    let age: Int
+}
+
+let foo = Person(
+    name: "Foo",
+    age: 20
+)
+let nameAndAge = foo.self
+
+struct CommodoreComputer {
+    let name: String
+    let manufacturer: String
+}
+
+let c64 = CommodoreComputer(
+    name: "My Commodore",
+    manufacturer: "Commodore"
+)
+
+let c128 = CommodoreComputer(
+    name: "My Commodore 128",
+    manufacturer: "Commodore"
+)
