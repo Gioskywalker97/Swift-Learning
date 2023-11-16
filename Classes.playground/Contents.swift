9c56b7d03c09754312c4d5acaf3a3a25b95be89b
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2023-11-16 17:32:48 -0500

    Started classes

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -1,2 +1,22 @@
 import Foundation
 
+//Creates Constructor/Initializer Implicitly for structs
+//struct Person {
+//    var name: String
+//    var age: Int
+//}
+
+//Constructor/Initializer Explicitly Syntax
+class Person {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func increaseAge() {
+        self.age += 1
+    }
+}
